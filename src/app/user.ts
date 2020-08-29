09b9af5e16c65b373236fa0e9f991fd0332d49df
Teddie <teddymachoka98@yahoo.com>
2020-08-29 10:44:01 +0300

    add user

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,10 @@
-export class User {
+export class user {
+    created_at: string;
+    avatar_url: any;
+    followers_url: number;
+    following_url: number;
+    repos_url: string;
+    login: number;
+    public_repos: string;
+   //  created_at: string;
 }
