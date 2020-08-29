947078f158ae599aee51a9082cf7978abf606ac2
Teddie <teddymachoka98@yahoo.com>
2020-08-29 10:29:49 +0300

    add github.spec.ts changes

--- src/app/github.spec.ts
+++ src/app/github.spec.ts
@@ -1,7 +1,12 @@
-import { Github } from './github';
+import { TestBed } from '@angular/core/testing';
 
-describe('Github', () => {
-  it('should create an instance', () => {
-    expect(new Github()).toBeTruthy();
+import { GithubService } from './github.service';
+
+describe('GithubService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: GithubService = TestBed.get(GithubService);
+    expect(service).toBeTruthy();
   });
 });
