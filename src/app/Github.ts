ef3fb0114f2dabb9187d45e815d879989431a481
Teddie <teddymachoka98@yahoo.com>
2020-08-29 11:19:37 +0300

    add changes

--- src/app/Github.ts
+++ src/app/Github.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { repos } from './repos';
+import { Observable } from 'rxjs';
 @Injectable()
 
 
